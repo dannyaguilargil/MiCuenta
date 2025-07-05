4d2ae71cb26e66263d4c26bfb3d65f117b77cf6a
dannyaguilargil <danny.aguilar.gil@gmail.com>
2025-07-05 09:56:15 -0500

    test smtp

--- micuenta/micuenta/settings.py
+++ micuenta/micuenta/settings.py
@@ -14,15 +14,12 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+from decouple import config
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-9e)!h0&q!7cfhcpn6m3&y@!2r8$b#u8zaf)%$uygw-)a)!*^fg'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost']
@@ -147,3 +144,12 @@ JAZZMIN_SETTINGS = {
     
     },
 }
+
+###CONFIGURACION DE CORREO
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
